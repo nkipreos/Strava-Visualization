b78840c44aeefae8e9b7607c94c7511e7a947a17
Nicolas Kipreos <nkipreos@gmail.com>
2017-01-24 14:34:28 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+files = Dir['db/seeds/strava/*.route']
+
+files.each do |file|
+  hash = eval(File.open(file).read)
+  Activity.create({
+    strava_id: hash['id'],
+    name: hash['name'],
+    start_lat: hash['start_latlng'][0],
+    start_lng: hash['start_latlng'][1],
+    end_lat: hash['end_latlng'][0],
+    end_lng: hash['end_latlng'][1],
+    total_elevation_gain: hash['total_elevation_gain'],
+    moving_time: hash['moving_time'],
+    distance: hash['distance'],
+    polyline: hash['map']['polyline'],
+    summary_polyline: hash['map']['summary_polyline']
+    })
+end
