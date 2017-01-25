ed8e7677ac25ae1715bbf93f7c499694ecd56f68
Nicolas Kipreos <nkipreos@gmail.com>
2017-01-25 16:39:42 -0300

    add route visualization as polyline

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,2 +1,8 @@
 class LandingController < ApplicationController
+  def index
+    @activities = Activity.all
+    @latlngs = []
+    @centerlat = (@activities.map {|x| x['start_lat']}.inject(:+)/@activities.count).to_f
+    @centerlng = (@activities.map {|x| x['start_lng']}.inject(:+)/@activities.count).to_f
+  end
 end
